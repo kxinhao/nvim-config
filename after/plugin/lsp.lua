710b820138b9bac09f134c55124c6492d3a481e3
kxinhao <kxinhao@gmail.com>
2023-12-30 16:29:27 +0800

    added remap for paste from system

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls', 'eslint', 'gopls'},
+  ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls', 'eslint', 'gopls', 'jdtls'},
   handlers = {
     lsp.default_setup,
   },
